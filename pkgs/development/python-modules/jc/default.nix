9ee6e4c9973b7bf78828fef97bdf3c99e3779466
Jonathan Ringer <jonringer117@gmail.com>
2021-12-07 00:56:09 -0800

    python3Packages.jc: 1.17.1 -> 1.17.3

--- pkgs/development/python-modules/jc/default.nix
+++ pkgs/development/python-modules/jc/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "jc";
-  version = "1.17.1";
+  version = "1.17.3";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "kellyjonbrazil";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-ISggj6oOF0B7TKIQAlZtauRrDAWP88OOFezLJK6edjI=";
+    sha256 = "0jnk61a4c8pvbxcfjlz90xapj5xggzgi3i2x208msr6qmah9z1rf";
   };
 
   propagatedBuildInputs = [ ruamel-yaml xmltodict pygments ];
